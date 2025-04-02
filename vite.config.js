895ef766d39703f5facf5c96d63e021b7cfb6eed
Sabarish575 <sabarish2k3@gmail.com>
2025-04-02 19:08:07 +0530

    Added

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "Rate_component", // Add this line
+  base: "/Rate_component/", // Add this line
 });
